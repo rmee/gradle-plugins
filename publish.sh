15db5d0d23ff326b09ef177e90af46504f75889c
remmeier <remo.meier@adnovum.ch>
2018-06-04 17:36:20 +0200

    publish kubernetes

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew :systemd-application:publishPlugins
+  ./gradlew :kubernetes:publishPlugins
 else
   echo "Skip publishing";
 fi

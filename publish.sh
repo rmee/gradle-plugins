7e28093026b53bef7b337449119e2306f564ea79
Remo <remo.meier@adnovum.ch>
2018-06-28 08:59:07 +0200

    support to publish everything

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "stable" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew :publishPlugins
+  ./gradlew publishPlugins
 elif [ "$TRAVIS_BRANCH" == "stable-systemd" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
   ./gradlew :systemd:publishPlugins
 elif [ "$TRAVIS_BRANCH" == "stable-build-on-change" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then

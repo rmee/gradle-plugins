3b0c8b45e81eb96c6bf8e168cf0db9f6cfce4e6e
Remo <remo.meier@adnovum.ch>
2018-06-28 13:19:04 +0200

    individual publishing of plugins

--- publish.sh
+++ publish.sh
@@ -5,6 +5,16 @@ elif [ "$TRAVIS_BRANCH" == "stable-systemd" -a "$TRAVIS_PULL_REQUEST" == "false"
   ./gradlew :systemd:publishPlugins
 elif [ "$TRAVIS_BRANCH" == "stable-build-on-change" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
   ./gradlew :build-on-change:publishPlugins
+elif [ "$TRAVIS_BRANCH" == "stable-oc" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  ./gradlew :oc:publishPlugins
+elif [ "$TRAVIS_BRANCH" == "stable-az" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  ./gradlew :az:publishPlugins
+elif [ "$TRAVIS_BRANCH" == "stable-helm" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  ./gradlew :helm:publishPlugins
+elif [ "$TRAVIS_BRANCH" == "stable-terraform" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  ./gradlew :helm:publishPlugins
+elif [ "$TRAVIS_BRANCH" == "stable-kubectl" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  ./gradlew :helm:publishPlugins
 else
   echo "Skip deploying";
 fi

2bef6a7b246c36e41ab75749a235c162b33e4d33
keykey7 <29210485+keykey7@users.noreply.github.com>
2018-09-25 17:10:06 +0200

    up-to-date checks for GCloudSetProjectTask and GCloudGetKubernetesCredentialsTask

--- gcloud/src/main/java/com/github/rmee/gcloud/GCloudSetProjectTask.java
+++ gcloud/src/main/java/com/github/rmee/gcloud/GCloudSetProjectTask.java
@@ -1,26 +1,32 @@
 package com.github.rmee.gcloud;
 
+import java.io.File;
+
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
 public class GCloudSetProjectTask extends DefaultTask {
 
+	private GCloudExtension getExtension() {
+		return getProject().getExtensions().getByType(GCloudExtension.class);
+	}
+
+	@Input
+	public String getProjectName() {
+		return getExtension().getProject();
+	}
+
+	@OutputFile
+	public File getConfigDefaultFile() {
+		return getProject().file("build/wrapper/.config/gcloud/configurations/config_default" );
+	}
+
 	@TaskAction
 	public void run() {
-		GCloudExtension extension = getProject().getExtensions().getByType(GCloudExtension.class);
-
-		String project = extension.getProject();
-		if (project == null) {
-			throw new IllegalStateException("gcloud.project not configured");
-		}
-
-		StringBuilder command = new StringBuilder();
-		command.append("gcloud config set project ");
-		command.append(project);
-
 		GCloudExecSpec execSpec = new GCloudExecSpec();
-		execSpec.setCommandLine(command.toString());
-		extension.exec(execSpec);
-
+		execSpec.setCommandLine("gcloud config set project " + getProjectName());
+		getExtension().exec(execSpec);
 	}
 }
--- gcloud/src/main/java/com/github/rmee/gcloud/gke/GCloudGetKubernetesCredentialsTask.java
+++ gcloud/src/main/java/com/github/rmee/gcloud/gke/GCloudGetKubernetesCredentialsTask.java
@@ -1,45 +1,64 @@
 package com.github.rmee.gcloud.gke;
 
+import java.io.File;
+
 import com.github.rmee.gcloud.GCloudExecSpec;
 import com.github.rmee.gcloud.GCloudExtension;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
 /**
  */
 public class GCloudGetKubernetesCredentialsTask extends DefaultTask {
 
+	private GCloudExtension getExtension() {
+		return getProject().getExtensions().getByType(GCloudExtension.class);
+	}
+
+	@Optional
+	@Input
+	public String getRegion() {
+		return getExtension().getRegion();
+	}
+
+	@Input
+	public String getZone() {
+		return getExtension().getZone();
+	}
+
+	@Input
+	public String getClusterName() {
+		return getExtension().getGke().getClusterName();
+	}
+
+	@OutputFile
+	public File getConfigFile() {
+		return getProject().file("build/wrapper/.kube/config" );
+	}
+
 	@TaskAction
 	public void run() {
 		// az aks get-credentials --resource-group myResourceGroup --name myAKSClusterö
-		GCloudExtension extension = getProject().getExtensions().getByType(GCloudExtension.class);
-
-		String region = extension.getRegion();
-		String zone = extension.getZone();
-		String clusterName = extension.getGke().getClusterName();
-		if(zone == null){
-			throw new IllegalStateException("gcloud.zone not configured");
-		}
-
 		StringBuilder builder = new StringBuilder();
 		builder.append("gcloud container clusters get-credentials ");
-		builder.append(clusterName);
+		builder.append(getClusterName());
 
-
-
-
+		String region = getRegion();
 		if(region != null) {
 			builder.append(" --region=");
 			builder.append(region);
 		}
 
 		builder.append(" --zone=");
-		builder.append(zone);
+		builder.append(getZone());
 
 		GCloudExecSpec execSpec = new GCloudExecSpec();
 		execSpec.setCommandLine(builder.toString());
 		//execSpec.setStdoutFile(tempFile);
-		extension.exec(execSpec);
+		getExtension().exec(execSpec);
 
 
 

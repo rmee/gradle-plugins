2c7b8a6a9d3c570ac77f6b78e3fc8b5f5a5d5482
Remo <remo.meier@adnovum.ch>
2018-11-01 14:55:50 +0100

    skip gradle publishing for testing

--- publish.sh
+++ publish.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "stable" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew publishPlugins
-elif [ "$TRAVIS_BRANCH" == "stable-systemd" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew :systemd:publishPlugins
-elif [ "$TRAVIS_BRANCH" == "stable-build-on-change" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew :build-on-change:publishPlugins
-elif [ "$TRAVIS_BRANCH" == "stable-oc" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew :oc:publishPlugins
-elif [ "$TRAVIS_BRANCH" == "stable-az" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew :az:publishPlugins
-elif [ "$TRAVIS_BRANCH" == "stable-helm" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew :helm:publishPlugins
-elif [ "$TRAVIS_BRANCH" == "stable-terraform" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew :helm:publishPlugins
-elif [ "$TRAVIS_BRANCH" == "stable-kubectl" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew :helm:publishPlugins
-else
-  echo "Skip deploying";
-fi
 
 if [ "$TRAVIS_BRANCH" == "stable" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
   ./gradlew publish --max-workers=1 --no-parallel -Pstable=true

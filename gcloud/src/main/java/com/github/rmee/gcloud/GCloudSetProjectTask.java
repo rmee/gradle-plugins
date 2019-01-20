68455ca4e243d1b5fd46f567c293314bc0d900c1
Remo Meier <remo.meier@adnovum.ch>
2019-01-20 21:38:33 +0100

    removed broken up-to-date check for gcloud plugin

--- gcloud/src/main/java/com/github/rmee/gcloud/GCloudSetProjectTask.java
+++ gcloud/src/main/java/com/github/rmee/gcloud/GCloudSetProjectTask.java
@@ -1,32 +1,30 @@
 package com.github.rmee.gcloud;
 
-import java.io.File;
-
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
 public class GCloudSetProjectTask extends DefaultTask {
 
-	private GCloudExtension getExtension() {
-		return getProject().getExtensions().getByType(GCloudExtension.class);
-	}
+    private GCloudExtension getExtension() {
+        return getProject().getExtensions().getByType(GCloudExtension.class);
+    }
 
-	@Input
-	public String getProjectName() {
-		return getExtension().getProject();
-	}
+    @Input
+    public String getProjectName() {
+        return getExtension().getProject();
+    }
 
-	@OutputFile
-	public File getConfigDefaultFile() {
-		return getProject().file("build/wrapper/.config/gcloud/configurations/config_default" );
-	}
+    // TODO deletes config_default from time to time => find better way
+    // @OutputFile
+    //public File getConfigDefaultFile() {
+    //	return getProject().file("build/wrapper/.config/gcloud/configurations/config_default" );
+    //}
 
-	@TaskAction
-	public void run() {
-		GCloudExecSpec execSpec = new GCloudExecSpec();
-		execSpec.setCommandLine("gcloud config set project " + getProjectName());
-		getExtension().exec(execSpec);
-	}
+    @TaskAction
+    public void run() {
+        GCloudExecSpec execSpec = new GCloudExecSpec();
+        execSpec.setCommandLine("gcloud config set project " + getProjectName());
+        getExtension().exec(execSpec);
+    }
 }

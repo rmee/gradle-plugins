3bb8d75d7ce574125fe337fef79ebb06477190bf
Remo <remo.meier@adnovum.ch>
2019-04-09 16:49:04 +0200

    allow to skip refresh for helmInit
    will prevent a connection to public helm repositories

--- helm/src/main/java/com/github/rmee/helm/HelmInit.java
+++ helm/src/main/java/com/github/rmee/helm/HelmInit.java
@@ -5,15 +5,41 @@ import org.gradle.api.tasks.TaskAction;
 
 public class HelmInit extends DefaultTask {
 
+	private boolean skipRefresh;
+
+	private String commandLine;
+
 	public HelmInit() {
 		setGroup("kubernetes");
 	}
 
 	@TaskAction
 	public void exec() {
+		if (commandLine == null) {
+			commandLine = "helm init --client-only";
+			if (skipRefresh) {
+				commandLine += " --skip-refresh";
+			}
+		}
 		HelmExtension extension = getProject().getExtensions().getByType(HelmExtension.class);
 		HelmExecSpec spec = new HelmExecSpec();
-		spec.setCommandLine("helm init --client-only");
+		spec.setCommandLine(commandLine);
 		extension.exec(spec);
+	}
+
+	public String getCommandLine() {
+		return commandLine;
+	}
+
+	public void setCommandLine(String commandLine) {
+		this.commandLine = commandLine;
+	}
+
+	public boolean isSkipRefresh() {
+		return skipRefresh;
+	}
+
+	public void setSkipRefresh(boolean skipRefresh) {
+		this.skipRefresh = skipRefresh;
 	}
 }

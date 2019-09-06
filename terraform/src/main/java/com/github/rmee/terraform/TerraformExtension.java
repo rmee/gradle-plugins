2903e3f80eea3888a750fb0a2f203a9b1473ca28
Remo <remo.meier@adnovum.ch>
2019-09-06 13:47:48 +0200

    terraform: minor refinements
    - allow to access console output (e.g. to get terraform output variables)
    - reconfigure working dir to src/main/terraform due to limitations/inconsistencies in Terraform CLI
    - remove uncessary volume mappings

--- terraform/src/main/java/com/github/rmee/terraform/TerraformExtension.java
+++ terraform/src/main/java/com/github/rmee/terraform/TerraformExtension.java
@@ -3,11 +3,15 @@ package com.github.rmee.terraform;
 import com.github.rmee.cli.base.Cli;
 import com.github.rmee.cli.base.CliExecSpec;
 import com.github.rmee.cli.base.ClientExtensionBase;
+import com.github.rmee.cli.base.ExecResult;
+import com.github.rmee.cli.base.OutputFormat;
 import com.github.rmee.cli.base.internal.CliDownloadStrategy;
 import groovy.lang.Closure;
 import org.gradle.api.Project;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,105 +19,117 @@ import java.util.stream.Collectors;
 
 public class TerraformExtension extends ClientExtensionBase {
 
-    private File sourceDir;
+	private File sourceDir;
 
-    private boolean debug = false;
+	private boolean debug = false;
 
-    private Map<String, Object> variables = new HashMap<>();
+	private Map<String, Object> variables = new HashMap<>();
 
-    public TerraformExtension() {
-        CliDownloadStrategy downloadStratey = new CliDownloadStrategy() {
-            @Override
-            public String computeDownloadFileName(Cli cli) {
-                throw new UnsupportedOperationException("download not  yet supported, make use of docker");
-            }
+	public TerraformExtension() {
+		CliDownloadStrategy downloadStratey = new CliDownloadStrategy() {
+			@Override
+			public String computeDownloadFileName(Cli cli) {
+				throw new UnsupportedOperationException("download not  yet supported, make use of docker");
+			}
 
-            @Override
-            public String computeDownloadUrl(Cli cli, String repository, String downloadFileName) {
-                throw new UnsupportedOperationException("download not  yet supported, make use of docker");
-            }
-        };
-        cli = new Cli(this, "terraform", downloadStratey);
-    }
+			@Override
+			public String computeDownloadUrl(Cli cli, String repository, String downloadFileName) {
+				throw new UnsupportedOperationException("download not  yet supported, make use of docker");
+			}
+		};
+		cli = new Cli(this, "terraform", downloadStratey);
+	}
 
-    public void init() {
-        if (initialized) {
-            return;
-        }
-        initialized = true;
-        this.cli.init(project);
-    }
+	public void init() {
+		if (initialized) {
+			return;
+		}
+		initialized = true;
+		this.cli.init(project);
+	}
 
-    public void exec(TerraformExecSpec spec) {
-        project.getLogger().warn("Executing: " + spec.getCommandLine().stream().collect(Collectors.joining(" ")));
+	public ExecResult exec(TerraformExecSpec spec) {
+		try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
 
-        project.exec(execSpec -> {
+			project.getLogger().warn("Executing: " + spec.getCommandLine().stream().collect(Collectors.joining(" ")));
 
-            // we mount the project into the docker container
-            File terraformTempDir = new File(project.getBuildDir(), "terraform");
-            terraformTempDir.mkdirs();
+			project.exec(execSpec -> {
 
-            final CliExecSpec duplicate = spec.duplicate();
-            List<String> commandLine = duplicate.getCommandLine();
+				// we mount the project into the docker container
+				File terraformTempDir = new File(project.getBuildDir(), "terraform");
+				terraformTempDir.mkdirs();
 
-            if (debug) {
-                commandLine.add("-e");
-                commandLine.add("TF_LOG=DEBUG");
-            }
+				final CliExecSpec duplicate = spec.duplicate();
+				List<String> commandLine = duplicate.getCommandLine();
 
-            TerraformExtension extension = project.getExtensions().getByType(TerraformExtension.class);
-            if (spec.getAddVariables()) {
-                for (Map.Entry<String, Object> entry : extension.getVariables().entrySet()) {
-                    Object value = entry.getValue();
-                    if (value instanceof Closure) {
-                        value = ((Closure) value).call();
-                    }
-                    if (value != null) {
-                        commandLine.add("-var");
-                        commandLine.add(entry.getKey() + "=" + value);
-                    }
-                }
-            }
+				if (debug) {
+					commandLine.add("-e");
+					commandLine.add("TF_LOG=DEBUG");
+				}
 
-            if (spec.getAddConfigDirectory()) {
-                commandLine.add("/etc/project/conf");
-            }
+				if (spec.getOutputFormat() != OutputFormat.CONSOLE) {
+					execSpec.setStandardOutput(outputStream);
+				}
 
-            cli.configureExec(execSpec, duplicate);
-        });
-    }
+				TerraformExtension extension = project.getExtensions().getByType(TerraformExtension.class);
+				if (spec.getAddVariables()) {
+					for (Map.Entry<String, Object> entry : extension.getVariables().entrySet()) {
+						Object value = entry.getValue();
+						if (value instanceof Closure) {
+							value = ((Closure) value).call();
+						}
+						if (value != null) {
+							commandLine.add("-var");
+							commandLine.add(entry.getKey() + "=" + value);
+						}
+					}
+				}
 
-    public Map<String, Object> getVariables() {
-        return variables;
-    }
+				if (spec.getAddConfigDirectory()) {
+					commandLine.add("/workdir/src/main/terraform");
+				}
 
-    public void setVariables(Map<String, Object> variables) {
-        this.variables = variables;
-    }
+				cli.configureExec(execSpec, duplicate);
+			});
 
-    public File getSourceDir() {
-        return sourceDir;
-    }
+			String output = outputStream.toString();
+			return new ExecResult(output);
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+	}
 
-    public void setSourceDir(File sourceDir) {
-        this.sourceDir = sourceDir;
-    }
+	public Map<String, Object> getVariables() {
+		return variables;
+	}
 
-    public boolean getDebug() {
-        return debug;
-    }
+	public void setVariables(Map<String, Object> variables) {
+		this.variables = variables;
+	}
 
-    public void setDebug(boolean debug) {
-        this.debug = debug;
-    }
+	public File getSourceDir() {
+		return sourceDir;
+	}
 
-    protected void setProject(Project project) {
-        this.project = project;
-    }
+	public void setSourceDir(File sourceDir) {
+		this.sourceDir = sourceDir;
+	}
 
-    public void exec(Closure<TerraformExecSpec> closure) {
-        TerraformExecSpec spec = new TerraformExecSpec();
-        project.configure(spec, closure);
-        exec(spec);
-    }
+	public boolean getDebug() {
+		return debug;
+	}
+
+	public void setDebug(boolean debug) {
+		this.debug = debug;
+	}
+
+	protected void setProject(Project project) {
+		this.project = project;
+	}
+
+	public ExecResult exec(Closure<TerraformExecSpec> closure) {
+		TerraformExecSpec spec = new TerraformExecSpec();
+		project.configure(spec, closure);
+		return exec(spec);
+	}
 }
--- terraform/src/main/java/com/github/rmee/terraform/TerraformPlugin.java
+++ terraform/src/main/java/com/github/rmee/terraform/TerraformPlugin.java
@@ -9,44 +9,44 @@ import java.io.File;
 
 public class TerraformPlugin implements Plugin<Project> {
 
-    public void apply(Project project) {
-        project.getPlugins().apply(CliExecPlugin.class);
+	public void apply(Project project) {
+		project.getPlugins().apply(CliExecPlugin.class);
 
-        File configDir = new File(project.getProjectDir(), "src/main/terraform");
-        TerraformExtension extension = project.getExtensions().create("terraform", TerraformExtension.class);
-        extension.setProject(project);
-        extension.setSourceDir(configDir);
-        extension.getCli().setImageName("hashicorp/terraform");
-        extension.getCli().setVersion("0.11.7");
+		File configDir = new File(project.getProjectDir(), "src/main/terraform");
+		TerraformExtension extension = project.getExtensions().create("terraform", TerraformExtension.class);
+		extension.setProject(project);
+		extension.setSourceDir(configDir);
+		extension.getCli().setImageName("hashicorp/terraform");
+		extension.getCli().setVersion("0.11.7");
+		extension.getCli().setWorkingDir("/workdir/src/main/terraform");
 
-        TerraformInitTask initTask = project.getTasks().create("terraformInit", TerraformInitTask.class);
-        TerraformValidateTask validateTask = project.getTasks().create("terraformValidate", TerraformValidateTask.class);
-        TerraformRefreshTask refreshTask = project.getTasks().create("terraformRefresh", TerraformRefreshTask.class);
-        TerraformPlanTask planTask = project.getTasks().create("terraformPlan", TerraformPlanTask.class);
-        TerraformApplyTask applyTask = project.getTasks().create("terraformApply", TerraformApplyTask.class);
-        TerraformDestroyTask destroyTask = project.getTasks().create("terraformDestroy", TerraformDestroyTask.class);
+		TerraformInitTask initTask = project.getTasks().create("terraformInit", TerraformInitTask.class);
+		TerraformValidateTask validateTask = project.getTasks().create("terraformValidate", TerraformValidateTask.class);
+		TerraformRefreshTask refreshTask = project.getTasks().create("terraformRefresh", TerraformRefreshTask.class);
+		TerraformPlanTask planTask = project.getTasks().create("terraformPlan", TerraformPlanTask.class);
+		TerraformApplyTask applyTask = project.getTasks().create("terraformApply", TerraformApplyTask.class);
+		TerraformDestroyTask destroyTask = project.getTasks().create("terraformDestroy", TerraformDestroyTask.class);
 
-        refreshTask.dependsOn(initTask);
-        validateTask.dependsOn(initTask);
-        planTask.dependsOn(validateTask);
-        applyTask.dependsOn(planTask);
-        destroyTask.dependsOn(planTask);
+		refreshTask.dependsOn(initTask);
+		validateTask.dependsOn(initTask);
+		planTask.dependsOn(validateTask);
+		applyTask.dependsOn(planTask);
+		destroyTask.dependsOn(planTask);
 
-        project.afterEvaluate(project1 -> {
-            Cli cli = extension.getCli();
-            cli.setupWrapper(project, false);
+		project.afterEvaluate(project1 -> {
+			Cli cli = extension.getCli();
+			cli.setupWrapper(project, false);
 
-            cli.addDefaultMappings(project);
+			cli.addDefaultMappings(project);
 
-            File terraformTempDir2 = new File(project.getBuildDir(), ".terraform");
-            cli.getVolumeMappings().put("/etc/project/conf", extension.getSourceDir());
-            cli.getVolumeMappings().put("/.terraform", terraformTempDir2);
+			File terraformTempDir2 = new File(project.getBuildDir(), ".terraformCache");
+			cli.getVolumeMappings().put("/.terraform", terraformTempDir2);
 
-            if (extension.getDebug()) {
-                cli.getEnvironment().put("TF_LOG", "DEBUG");
-            }
-        });
-    }
+			if (extension.getDebug()) {
+				cli.getEnvironment().put("TF_LOG", "DEBUG");
+			}
+		});
+	}
 }
 
 
